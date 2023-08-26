082bbd17bc50a16923e3e2b0302300b95154cfad
Bostigger <tricksteck@gmail.com>
2023-08-26 19:39:33 +0000

    blockchain complete

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -21,8 +23,8 @@ type Book struct {
 type Block struct {
 	Data         BookCheckout
 	PreviousHash string
-	NextHash     string
-	Position     string
+	Hash         string
+	Position     int
 	TimeStamp    string
 }
 type Blockchain struct {
@@ -36,17 +38,82 @@ type BookCheckout struct {
 	IsGenesis    bool      `json:"is_genesis"`
 }
 
-//var Blockchain *Blockchain
+var BlockChain *Blockchain
 
-func (bc *Blockchain) AddBlock() {
-
+func (b *Block) generateHash() {
+	bytes, _ := json.Marshal(b.Data)
+	data := string(b.Position) + b.TimeStamp + string(bytes) + b.PreviousHash
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	b.Hash = hex.EncodeToString(hash.Sum(nil))
 }
 
+func (bc *Blockchain) AddBlock(data BookCheckout) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	block := CreateBlock(prevBlock, data)
+	if validBlock(block, prevBlock) {
+		bc.blocks = append(bc.blocks, block)
+	}
+}
+
+func CreateBlock(preBlock *Block, data BookCheckout) *Block {
+	block := &Block{}
+	block.Data = data
+	block.Position = preBlock.Position + 1
+	block.PreviousHash = preBlock.Hash
+	block.TimeStamp = time.Now().String()
+	block.generateHash()
+	return block
+}
+
+func validBlock(block, prevBlock *Block) bool {
+	if block.PreviousHash != prevBlock.Hash {
+		return false
+	}
+	if !block.validateHash(block.Hash) {
+		return false
+	}
+	if prevBlock.Position+1 != block.Position {
+		return false
+	}
+	return true
+}
+
+func (block *Block) validateHash(hash string) bool {
+	block.generateHash()
+	if block.Hash != hash {
+		return false
+	}
+	return true
+}
+
+func NewBlockChain() *Blockchain {
+	return &Blockchain{
+		[]*Block{GenesisBlock()},
+	}
+}
+
+func GenesisBlock() *Block {
+	return CreateBlock(&Block{}, BookCheckout{IsGenesis: true})
+}
 func main() {
+	BlockChain = NewBlockChain()
 	r := mux.NewRouter()
 	r.HandleFunc("/new", newBook).Methods("POST")
 	r.HandleFunc("/block", writeBlock).Methods("POST")
 	r.HandleFunc("/", getBlockChain).Methods("GET")
+
+	go func() {
+		for _, block := range BlockChain.blocks {
+			fmt.Printf("Prev.Hash : %x\n", block.PreviousHash)
+			bytes, _ := json.MarshalIndent(block.Data, "", "")
+			fmt.Printf("Data %v\n", string(bytes))
+			fmt.Printf("Hash %v\n", block.Hash)
+			fmt.Println()
+
+		}
+	}()
+
 	log.Println("server listening on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
@@ -80,10 +147,19 @@ func writeBlock(w http.ResponseWriter, r *http.Request) {
 	var checkOutItem BookCheckout
 	if err := json.NewDecoder(r.Body).Decode(&checkOutItem); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error decoding the book checkout "))
+		log.Println(err)
+		w.Write([]byte("error writing block "))
 		return
 	}
+
+	BlockChain.AddBlock(checkOutItem)
 }
 func getBlockChain(w http.ResponseWriter, r *http.Request) {
-
+	bcbytes, err := json.MarshalIndent(BlockChain.blocks, "", "")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	io.WriteString(w, string(bcbytes))
 }
